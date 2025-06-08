cf30d681c3334c4ee7d15151387b44d542f1c9df
em3459 <119pubgwarrior@gmail.com>
2025-06-08 01:00:43 -0700

    Update App.jsx

--- YtReactNative/App.jsx
+++ YtReactNative/App.jsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
 import React from 'react'
 
 const App = () => {
 
   //js object style-2
   return (
-    <View /* intext style-1 */>
-      <Text>###ssssss##</Text>
-    </View>
+    <SafeAreaView /* intext style-1 */>
+      <Text>###ssssffffss##</Text>
+    </SafeAreaView>
   )
 }
 
