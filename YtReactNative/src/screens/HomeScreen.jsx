ffb97d12cc9f049e8c2b96fc01e4563c7d695267
em3459 <119pubgwarrior@gmail.com>
2025-06-12 17:37:29 -0700

    Update HomeScreen.jsx

--- YtReactNative/src/screens/HomeScreen.jsx
+++ YtReactNative/src/screens/HomeScreen.jsx
@@ -14,12 +14,13 @@ const App = () => {
 // Define your styles here
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#ffffff',
     width:"100%",
-    height:"100%"
+    height:"100%",
+    padding:"4%"
   },
   title: {
     fontSize: 24,
