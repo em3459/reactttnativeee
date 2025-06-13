68069f55dd6e7fdbc6d518733b33e8df05aa992d
em3459 <119pubgwarrior@gmail.com>
2025-06-13 09:13:38 -0700

    Update HomeScreen.jsx

--- YtReactNative/src/screens/HomeScreen.jsx
+++ YtReactNative/src/screens/HomeScreen.jsx
@@ -1,12 +1,23 @@
 // Import core libraries
 //import React from 'react';
-import { SafeAreaView, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, StatusBar, View, Pressable } from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Text style={styles.title}>Welcome to Sunny leone!</Text>
+      {/* <StatusBar barStyle="dark-content" /> */}
+      <Text style={styles.title}>Dashboard</Text>
+      <View style={styles.btnContainer}>
+        <Pressable style={styles.button}>
+            <Text style={styles.btntext}> All items</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+            <Text style={styles.btntext}> All items</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+            <Text style={styles.btntext}> All items</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
@@ -27,6 +38,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#333',
   },
+   btnContainer:{
+    flexDirection:"row",
+    gap:10,
+    marginVertical:10
+  },
+  
+  button:{
+    borderWidth:0.8,
+    borderColor:"green",
+    borderRadius:50,
+    paddingHorizental:50,
+    paddingVertical:5
+  },
+ 
+  btntext:{
+    color:"green",
+    fontsize:12
+
+  }
 });
 
 export default App;
