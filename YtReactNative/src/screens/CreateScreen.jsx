d04638bc7fd2ee2eb32cd78cd08a9ecb908909ed
em3459 <119pubgwarrior@gmail.com>
2025-06-18 11:36:12 -0700

    Create CreateScreen.jsx

---
+++ YtReactNative/src/screens/CreateScreen.jsx
@@ -0,0 +1,15 @@
+//rnfes
+import { StyleSheet, Text, View } from 'react-native'
+
+
+const CreateScreen = () => {
+  return (
+    <View>
+      <Text>CreateScreen</Text>
+    </View>
+  )
+}
+
+export default CreateScreen
+
+const styles = StyleSheet.create({})
